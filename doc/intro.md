c69892c5b8c800d440fdb2c924452c502e1eaaa9
Axel Knauf <axel.knauf@posteo.de>
2018-02-27 08:12:23 +0100

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to cib
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/cib/core_test.clj
@@ -0,0 +1,7 @@
+(ns cib.core-test
+  (:require [clojure.test :refer :all]
+            [cib.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
